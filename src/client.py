e723492005503263ba5e4d9d339d7c7098ae8714
sararo <sarierotenberg@gmail.com>
2018-01-15 10:45:19 +0200

    change to view the message

--- src/client.py
+++ src/client.py
@@ -5,7 +5,7 @@ import sys
 def main(message) :
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
-    my_socket.sendto("wearetherock", ('<broadcast>' ,8881))
+    my_socket.sendto(message, ('<broadcast>' ,8881))
     my_socket.close()
 
 
