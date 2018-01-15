a721ba7a04bff3758292e2062104d95333b0e398
reutnagar <salreut1@gmail.com>
2018-01-15 14:48:16 +0200

    Add async reading from socket
    so we can perform other operations in the middle

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 import socket
 import logging
 import sys
-#import dbus
+import network
+import time
 
 logger = 0
 
 def init_log():
     FORMAT = '%(asctime)-15s: %(message)s'
     logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-    # global logger
-    # logger = logging.getLogger('init')
 
 def init():
     pass
@@ -18,19 +17,24 @@ def main():
     init_log()
     init()
 
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    my_socket.bind(('',8881))
+    # my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # my_socket.bind(('',8881))
     logging.info('start service ...')
 
     try:
-        while True :
-            #my_socket.settimeout(5.0)
-            message , address = my_socket.recvfrom(8192)
-            logging.info('message %s from : %s' % ( str(message), address[0]))    
-
-    except KeyboardInterrupt:
+        # while True :
+        #     message , address = my_socket.recvfrom(8192)
+        #     logging.info('message %s from : %s' % ( str(message), address[0]))
+        client = network.start()    
+        x = 0
+        while True: # Async read from socket, can perform other operations here...
+            x = x+1
+            print 'number: %d' % (x)
+            time.sleep(1)
+    except KeyboardInterrupt:   # print all messages
+        print 'buffer: %s' %(str(client.buffer))
         sys.exit()
 
 if __name__ == "__main__" :
