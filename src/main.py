8c34a4c7405ff0a69d88cf22cf8aae3e07941e61
reutnagar <salreut1@gmail.com>
2018-01-15 12:57:01 +0200

    Add logging system
    using module 'logging'

--- src/main.py
+++ src/main.py
@@ -1,28 +1,37 @@
 import socket
+import logging
+import sys
 #import dbus
 
-# def show_message(title, message):
-#     bus = dbus.SessionBus()
-#     object = bus.get_object("org.gnome.Tomboy", "/org/gnome/Tomboy/RemoteControl")
-#     tomboy = dbus.Interface(object, "org.gnome.Tomboy.RemoteControl")
+logger = 0
 
-#     note = tomboy.CreateNamedNote(title)
-#     tomboy.SetNoteContents(note , message)
-#     tomboy.AddTagToNote(note,title)
-#     tomboy.DisplayNote(note)
+def init_log():
+    FORMAT = '%(asctime)-15s: %(message)s'
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+    # global logger
+    # logger = logging.getLogger('init')
+
+def init():
+    pass
 
 def main():
+    init_log()
+    init()
+
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     my_socket.bind(('',8881))
+    logging.info('start service ...')
 
-    print 'start service ...'
+    try:
+        while True :
+            #my_socket.settimeout(5.0)
+            message , address = my_socket.recvfrom(8192)
+            logging.info('message %s from : %s' % ( str(message), address[0]))    
 
-    while True :
-        message , address = my_socket.recvfrom(8192)
-        print 'message %s from : %s' % ( str(message), address[0])
-        #show_message('message from :'+ str(address[0]) , message)
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == "__main__" :
     main()
