18a4ff9ee4ad355ed801792afa6d31507649c6b4
reutnagar <salreut1@gmail.com>
2018-02-21 21:50:42 +0200

    Connection to master+handshake
    fixed some bugs. now working great!!

--- src/client.py
+++ src/client.py
@@ -20,16 +20,22 @@ class Client():
 	
 	def __init__(self):
 		self.data = None
-		self.strengh = 6	# self.calc_strengh()
-
+		self.strengh = 10	# self.calc_strengh()
+		self.sending_ip = ''
+		self.sending_msg = ''
+		self.msg_dict = {MSG_I_MASTER: self.proc_MSG_I_MASTER, MSG_YOU_MASTER: self.proc_MSG_YOU_MASTER, MSG_OK_I_MASTER: self.proc_MSG_OK_I_MASTER,}
+    	
 	def calc_strengh(self):
 		pass
 
 	def broadcast(self):
 		my_bc_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		my_bc_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-		while self.data.state is not STATE_MASTER:
+		while(True):
 			time.sleep(2)
+			if(self.data.state != STATE_TMP_CLIENT):
+				my_bc_socket.close()
+				return
 			my_bc_socket.sendto(MSG_I_MASTER, ('<broadcast>' ,8881))
 			print 'Sent broadcast message: %s' % MSG_I_MASTER
 		my_bc_socket.close()
@@ -64,46 +70,62 @@ class Client():
 			print 'Got message: %s. from : %s' % ( str(message), address[0])
 		return str(message), address[0]
 	
-	def proc_MSG_I_MASTER(self, ip):
+	def proc_MSG_I_MASTER(self):
+		ip = self.sending_ip
+		print 'in proc_MSG_I_MASTER'
 		self.data.state = STATE_CLIENT
+		self.data.master_ip = ip		
 		self.send_message(MSG_YOU_MASTER, ip)
 	
-	def proc_MSG_YOU_MASTER(self, ip):
-		if(self.data.state == STATE_CLIENT):
+	def proc_MSG_YOU_MASTER(self):
+		ip = self.sending_ip
+		print 'in proc_MSG_YOU_MASTER'
+		if(self.data.state != STATE_TMP_CLIENT or self.data.master_ip != ""):
 			return
-		self.data.state = STATE_MASTER
+		print 'in proc_MSG_YOU_MASTER'
+		self.data.state = STATE_MASTER # will couse exit the Client instance and create a Master in main
 		self.data.neighbors.append(ip)  # add the ip of the node to internal list. will send to all clients later		pass
 		self.send_message(MSG_OK_I_MASTER, ip)
 
-	def proc_MSG_OK_I_MASTER(self, ip):
-		self.data.master_ip = ip
+	def proc_MSG_OK_I_MASTER(self):
+		ip = self.sending_ip
+		print 'in proc_MSG_OK_I_MASTER'
+		if(self.data.master_ip != ip):
+			print 'Error! got MSG_OK_I_MASTER from new ip!'
+			self.data.state = STATE_ERROR  # this will re-create the device as client
+		#self.data.master_ip = ip
 		print 'Setting: %s as my Master...' % (str(ip))
 
-	def proc_MSG_UNDEFINED(self, ip):
-		print 'Undefined message frpm: %s. Ignore...' % (str(ip))
+	def proc_MSG_UNDEFINED(self):
+		ip = self.sending_ip
+		msg = self.sending_msg
+		print 'Undefined message: %s. from: %s. Ignore...' % (msg, str(ip))
 
 	def process_message(self, msg, ip):
 		if(ip == self.data.my_ip):
 			return
-		return {
-	        MSG_I_MASTER: self.proc_MSG_I_MASTER(ip),
-	        MSG_YOU_MASTER: self.proc_MSG_YOU_MASTER(ip),
-	        MSG_OK_I_MASTER: self.proc_MSG_OK_I_MASTER(ip),
-    	}.get(msg, self.proc_MSG_UNDEFINED(ip))
+		self.sending_ip = ip
+		self.sending_msg = msg
+		print 'in process_message. msg: %s' % msg
+		return self.msg_dict.get(msg, self.proc_MSG_UNDEFINED)()
 
 	def run(self,REQUIRED_STRENGH, my_state):
+		print 'Client.run() is started...'
+		# init internal data
 		self.data = my_state
 		self.data.state = STATE_CLIENT
 		if os.name != "nt":	# for Arduino
 			self.data.my_ip = get_interface_ip("apcli0")
 		else: 				# for Windows PC
-			self.data.my_ip = socket.gethostbyname(socket.gethostname())	
+			self.data.my_ip = socket.gethostbyname(socket.gethostname())
+		# broadcast if needed	
 		if(self.strengh > REQUIRED_STRENGH):
 			self.data.state = STATE_TMP_CLIENT
 			self.async_action(self.broadcast)
-		while((self.data.state is not STATE_MASTER) and (self.data.state is not STATE_ERROR)):
+		# listen & process messages
+		while((self.data.state != STATE_MASTER) and (self.data.state != STATE_ERROR)):
 			msg, ip = self.listen()
 			self.process_message(msg, ip)
-		if(self.data.state == STATE_MASTER):
-			return self.data
+			if(self.data.state == STATE_MASTER):
+				print 'my state == STATE_MASTER. returning to main...'
 		return self.data
