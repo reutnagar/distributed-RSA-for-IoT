0693974ece7a7a3fef89516a6864a96914802b1c
reutnagar <salreut1@gmail.com>
2018-04-26 21:15:43 +0300

    Starting the Async thread- work!!

--- src/messages.py
+++ src/messages.py
@@ -1,20 +1,71 @@
+import struct
+import threading
+import socket
+import time
+import os
 
-stateInt
+PORT = 8881
+stateInt = None
 
+if os.name != "nt":
+    import fcntl
+    #import struct
+
+    def get_interface_ip(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                ifname[:15]))[20:24])
+
+if os.name != "nt": # for Arduino
+    my_ip = get_interface_ip("apcli0")
+else:               # for Windows PC
+    my_ip = socket.gethostbyname(socket.gethostname())
+
+print("My IP is: " + my_ip)
+
+#############################
+#### Async thread  ##########
+#############################
 def async_listenToMessages(state):
+	global stateInt
 	print("Creating New thread..")
 	stateInt = state
-	c = threading.Thread(target=server, args=(9999,))
+	c = threading.Thread(target=asyncListen)  #, args=(9999,)
+	c.daemon = True
 	c.start()
-	time.sleep(1)
-	client(9999)
-	c.join()
-	print 'done'
-        # comm = threading.Thread(target=func_to_run)
-        # comm.daemon = True # the thread will be killed when main program exits
-        # comm.start()
 
-import struct
+def listen(socket):
+	message , address = socket.recvfrom(1024)
+	if(my_ip != address[0]): # Each device gets its own msg, because it is broadcast
+		print 'Got message: %s. from : %s' % ( str(message), address[0])
+	return str(message), address[0]
+
+
+def asyncListen():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(('',PORT))
+	while(True):  # may stop the thread on some condition...
+		msg, ip = listen(s)  # block until message accepted
+		process_message(message, ip)
+
+def process_message(message, ip):
+	if message == 'IS_THERE_MASTER' and state.status == 'MASTER_INIT or MASTER_DONE':
+		print 'send message I_AM_MASTER to ip'
+	else:
+		if message == 'I_AM_MASTER' and state.status == 'NODE_INIT':
+			state.status == 'MASTER_FOUND'
+			state.masterIP = ip
+		else: 
+			if message == 'I_AM_ON_THE_NETWORK' and ip not in status.neighbors:
+				status.neighbors.append(ip)
+			else:
+				if message == 'CLIENT_SUBSET_KEYS' and state.status == 'CLIENT_INIT':
+					print 'recieve the list of the keys'
+
+
+#############################
+#### External API ###########
+#############################
 
 def _get_block(s, count):
     if count <= 0:
@@ -35,7 +86,6 @@ def _send_block(s, data):
     while data:
         data = data[s.send(data):]
 
-#if False:
 def get_msg(s):
     count = struct.unpack('>i', _get_block(s, 4))[0]
     return _get_block(s, count)
@@ -46,85 +96,6 @@ def send_msg(s, data):
     _send_block(s, data)
     print '1'+header
     print '2'+data
-
-# if True:
-
-#     def _get_count(s):
-#         buf = ''
-#         while True:
-#             c = s.recv(1)
-#             if not c:
-#                 # error or just end of connection/
-#                 if buf:
-#                     raise RuntimeError("underflow")
-#                 else:
-#                     return -1
-#             if c == '|':
-#                 return int(buf)
-#             else:
-#                 buf += c
-
-#     def get_msg(s):
-#         return _get_block(s, _get_count(s))
-
-#     def send_msg(s, data):
-#         _send_block(s, str(len(data)) + '|')
-#         _send_block(s, data)
-
-
-import threading
-import socket
-import time
-import os
-
-if os.name != "nt":
-    import fcntl
-    import struct
-
-    def get_interface_ip(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
-                                ifname[:15]))[20:24])
-
-if os.name != "nt": # for Arduino
-    host = get_interface_ip("apcli0")
-else:               # for Windows PC
-    host = socket.gethostbyname(socket.gethostname())
-
-def client(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    print get_msg(s)
-    print get_msg(s)
-    s.shutdown(socket.SHUT_RDWR)
-    s.close()
-
-def process_message(message, ip):
-	if message == 'IS_THERE_MASTER' and state.status == 'MASTER_INIT or MASTER_DONE':
-		print 'send message I_AM_MASTER to ip'
-	else:
-		if message == 'I_AM_MASTER' and state.status == 'NODE_INIT':
-			state.status == 'MASTER_FOUND'
-			state.masterIP = ip
-		else: 
-			if message == 'I_AM_ON_THE_NETWORK' and ip not in status.neighbors:
-				status.neighbors.append(ip)
-			else:
-				if message == 'CLIENT_SUBSET_KEYS' and state.status == 'CLIENT_INIT':
-					print 'recieve the list of the keys'
-
-# def listen(self, socket):
-#     message , address = socket.recvfrom(1024)
-#     if(self.data.my_ip != address[0]): # Each device gets its own msg, because it is broadcast
-# 		print 'Got message: %s. from : %s' % ( str(message), address[0])
-#     return str(message), address[0]
-
-
-def server(port):
-	while(True):  // may stop the thread on some codition...
-		msg, ip = listen()  // block untill message accepted
-		process_message(message, ip)
-
 
 def broadcast(message): #send broadcast message
 	my_bc_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -139,6 +110,10 @@ def send_single_msg(message,ip):
    	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s.bind((host, port))
 	c, addr = s.accept()
-    send_msg(c, message)
-    c.close()
-    s.close()
+	send_msg(c, message)
+	c.close()
+	s.close()
+	
+	
+	
+	
