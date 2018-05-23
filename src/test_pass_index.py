a0155f126a9699f55b81f81292ff7f8d272df447
sararo <sarierotenberg@gmail.com>
2018-05-23 20:42:55 +0300

    testing - not finished...

--- src/test_pass_index.py
+++ src/test_pass_index.py
@@ -1,1 +1,7 @@
 import main
+
+#list_of_indexes = state.keys()
+list_of_indexes = [2,7,5,3,4,5,9,88]
+ip = '10.0.0.14'
+messages.send_single_msg(message.CLIENT_START_SESSION,0,list_of_indexes,ip)
+print('send list of indexes')
---
+++ src/test_recieve_index.py
@@ -0,0 +1,6 @@
+import main
+
+#list_of_indexes = state.keys()
+list_of_indexes = [1,4,7,9,33,5]
+ip = '10.0.0.1'
+print('recieve list of indexes')
