d85e6df56ce95b593572a48f70b049cc00c94a18
reutnagar <salreut1@gmail.com>
2018-06-18 03:43:36 +0300

    remove canIBeMaster var
    Co-Authored-By: sararo <sararo@users.noreply.github.com>

--- src/client.py
+++ src/client.py
@@ -6,20 +6,18 @@ import time, os
 
 def find_master():
 	print("Looking for the Master on the network...")
-	counter = 2 # two tries for looking after master, if there is a stack in the network. if can't be a master - asked until found
-	canIBeMaster = True # may be done with: calculateStrengh() function
+	counter = 2 # two tries for looking after master, if there is a stack in the network
 	while(state.status != MASTER_FOUND):
 		print("Trying for the #" + str(3-counter) + " time...")
 		messages.broadcast(messages.IS_THERE_MASTER) # ask if there is master on the network
 		time.sleep(10) # wait for master response (it takes a long time for the IoT to respond... ~10 seconds!)
-		if(canIBeMaster): # if the node can be a master
-			counter -= 1
-			if(counter == 0 and state.status != MASTER_FOUND): # after 2 tries to find the master, set myself as Master
-				messages.broadcast(messages.I_AM_MASTER)
-				state.masterIP = state.myIP
-				state.status =  MASTER_FOUND
-				print("No master is found! Setting myself as master")
-				return True
+		counter -= 1
+		if(counter == 0 and state.status != MASTER_FOUND): # after 2 tries to find the master, set myself as Master
+			messages.broadcast(messages.I_AM_MASTER)
+			state.masterIP = state.myIP
+			state.status =  MASTER_FOUND
+			print("No master is found! Setting myself as master")
+			return True
 	return False
 
 def publishMe():
