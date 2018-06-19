b6840e544dd62fa0cf877119c7531ade870974c5
reutnagar <salreut1@gmail.com>
2018-06-19 10:11:16 +0300

    Update test_pool.py

--- src/test_pool.py
+++ src/test_pool.py
@@ -46,7 +46,3 @@ print str(count*100.0/a)+'%'
 			# count+=1
 # print str(count) +'in'+ str(a)
 # print str(count*100.0/a)+'%'
-
-import test_pass_index
-import test_recieve_index
-import test_crypt
